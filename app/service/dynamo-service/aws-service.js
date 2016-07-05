1e403ee7cdee96b6e84d405b54814f8fb05d36d2
Marcelo Aymone <mbaymone@gmail.com>
2016-07-05 16:54:48 -0300

    Add dynamodb docker container

--- app/service/dynamo-service/aws-service.js
+++ app/service/dynamo-service/aws-service.js
@@ -9,7 +9,7 @@ function DynamoDB() {
         "accessKeyId": "ACCESS_KEY",
         "secretAccessKey": "SECRET_KEY",
         "region": "us-east-1",
-        "endpoint": "http://localhost:4761/"
+        "endpoint": "http://localhost:4770/"
     });
     return new AWS.DynamoDB();
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
 var sass = require('gulp-ruby-sass');
 var connect = require('gulp-connect');
 // requires browserify and vinyl-source-stream
@@ -8,14 +9,14 @@ var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
 
 // Connect task
-gulp.task('connect', () => {
+gulp.task('connect', function() {
     connect.server({
         root: 'public',
         port: 4000
     });
 });
 
-gulp.task('build', () => {
+gulp.task('build', function() {
     // Grabs the app.js file
     return browserify('./app/app.js')
         // bundles it and creates a file called main.js
@@ -27,8 +28,13 @@ gulp.task('build', () => {
         .pipe(gulp.dest('./public/js/'));
 });
 
-gulp.task('watch', () => {
+gulp.task('watch', function() {
     gulp.watch('app/**/*.*', ['build']);
 });
+
+//Dockerize dynamodb on 4770
+gulp.task('dockerize', shell.task([
+    'docker run -d -p 4770:4761 daime/docker-dynamodb:0.1.0'
+]));
 
 gulp.task('default', ['connect', 'watch']);
