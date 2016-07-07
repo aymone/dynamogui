9ed970f62a6fc7cd6f7dfc3fc9174d5cd9b70244
Marcelo Aymone <mbaymone@gmail.com>
2016-07-07 15:30:42 -0300

    Init credentials

--- app/service/dynamo-service/aws-service.js
+++ app/service/dynamo-service/aws-service.js
@@ -1,15 +1,46 @@
 var AWS = require('aws-sdk');
 
-angular
-    .module('AWSService', [])
-    .factory('DynamoDB', DynamoDB);
-
-function DynamoDB() {
-    AWS.config.update({
+var auths = {
+    default: {
         "accessKeyId": "ACCESS_KEY",
         "secretAccessKey": "SECRET_KEY",
         "region": "us-east-1",
         "endpoint": "http://localhost:4770/"
-    });
+    },
+    teste: {
+        "accessKeyId": "ACCESS_KEY",
+        "secretAccessKey": "SECRET_KEY",
+        "region": "us-east-1",
+        "endpoint": "http://localhost:4770/"
+    }
+};
+
+angular
+    .module('AWSService', [])
+    .factory('CredentialService', CredentialService)
+    .factory('DynamoDB', DynamoDB);
+
+
+DynamoDB.$inject = ['CredentialService'];
+function DynamoDB(CredentialService) {
+    var dynamo = false;
     return new AWS.DynamoDB();
+}
+
+CredentialService.$inject = ['$scope'];
+function CredentialService($scope) {
+    var connected = false;
+
+    function set(credential) {
+        AWS.config.update(credential);
+        return get();
+    }
+
+    function get() {
+        return AWS.config
+            .getCredentials(function(err) {
+                connected = err ? false : true;
+                return connected;
+            });
+    }
 }
